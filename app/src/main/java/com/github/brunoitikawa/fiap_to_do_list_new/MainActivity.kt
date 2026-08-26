49abab887b0fd23552493abb330aacd5d01445c1
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-25 21:54:33 -0300

    feat: Trabalho ToDoList

--- app/src/main/java/com/github/brunoitikawa/fiap_to_do_list_new/MainActivity.kt
+++ app/src/main/java/com/github/brunoitikawa/fiap_to_do_list_new/MainActivity.kt
@@ -11,7 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.github.brunoitikawa.fiap_to_do_list_new.navigation.AppNavigation
 import com.github.brunoitikawa.fiap_to_do_list_new.ui.theme.FiaptodolistnewTheme
+import com.github.brunoitikawa.fiap_to_do_list_new.viewmodel.TarefaViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +22,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FiaptodolistnewTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val viewModel: TarefaViewModel = viewModel(
+                    factory = TarefaViewModel.factory(applicationContext)
+                )
+                AppNavigation(viewModel = viewModel)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FiaptodolistnewTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/github/brunoitikawa/fiap_to_do_list_new/repository/TarefaRepository.kt
@@ -0,0 +1,17 @@
+package com.github.brunoitikawa.fiap_to_do_list_new.repository
+
+
+import com.github.brunoitikawa.fiap_to_do_list_new.data.Tarefa
+import com.github.brunoitikawa.fiap_to_do_list_new.data.TarefaDao
+import kotlinx.coroutines.flow.Flow
+
+class TarefaRepository(private val dao: TarefaDao) {
+
+    val tarefas: Flow<List<Tarefa>> = dao.listarTodas()
+
+    suspend fun inserir(tarefa: Tarefa) = dao.inserir(tarefa)
+
+    suspend fun atualizar(tarefa: Tarefa) = dao.atualizar(tarefa)
+
+    suspend fun deletar(tarefa: Tarefa) = dao.deletar(tarefa)
+}
